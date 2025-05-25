2c188c206c4542d29ad1756d93656e4d3ebfb7a8
jahid-cr7 <jahidhasan@stu.scu.edu.cn>
2025-05-26 05:14:33 +0800

    all done

--- src/Pages/ReadList.jsx/ReadList.jsx
+++ src/Pages/ReadList.jsx/ReadList.jsx
@@ -7,39 +7,73 @@ import ReadBook from "../../ReadBook/ReadBook";
 
 
 const ReadList = () => {
-    const [myreadlist, setreadlist] = useState([])
-  const data = useLoaderData();
-  useEffect(() => {
-    const storedBookData = getBook();
-    const convertedStoredBooks = storedBookData.map((id) => parseInt(id));
-    const readlist = data.filter((book) =>
-      convertedStoredBooks.includes(book.bookId)
-    );
-    setreadlist(readlist);
-  }, [data]);
-  return (
-    <div className="mt-5">
-      <Tabs>
-        <TabList>
-          <Tab>Read List</Tab>
-          <Tab>WishLists</Tab>
-        </TabList>
+    const [myreadlist, setreadlist] = useState([]);
+    const [sort, setSort] = useState("");
+    const data = useLoaderData();
+    useEffect(() => {
+      const storedBookData = getBook();
+      const convertedStoredBooks = storedBookData.map((id) => parseInt(id));
+      const readlist = data.filter((book) =>
+        convertedStoredBooks.includes(book.bookId)
+      );
+      setreadlist(readlist);
+    }, [data]);
 
-        <TabPanel>
-          <h2>Read Count : {myreadlist.length}</h2>
-          <div>
-            {
-                myreadlist.map((book) => 
-                <ReadBook book={book}></ReadBook>)
-            }
+    const handleSort = (type) => {
+      setSort(type);
+      if (type === "Pages") {
+        const sortedBypages = [...myreadlist].sort(
+          (a, b) => a.totalPages - b.totalPages
+        );
+        setreadlist(sortedBypages);
+        console.log(sortedBypages);
+      }
+      if (type === "Ratings") {
+        const sortedByRatings = [...myreadlist].sort(
+          (a, b) => a.rating - b.rating
+        );
+        setreadlist(sortedByRatings);
+      }
+    };
+
+    return (
+      <div className="mt-5">
+        <div className="dropdown dropdown-bottom mb-16">
+          <div tabIndex={0} role="button" className="btn m-1 ">
+            Sort By : {sort ? sort : ""}
           </div>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-      </Tabs>
-    </div>
-  );
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a onClick={() => handleSort("Pages")}>Pages</a>
+            </li>
+            <li>
+              <a onClick={() => handleSort("Ratings")}>Ratings</a>
+            </li>
+          </ul>
+        </div>
+
+        <Tabs>
+          <TabList>
+            <Tab>Read List</Tab>
+            <Tab>WishLists</Tab>
+          </TabList>
+
+          <TabPanel>
+            <div className="mt-10">
+              {myreadlist.map((book) => (
+                <ReadBook book={book} key={book.bookId}></ReadBook>
+              ))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
+    );
 };
 
 export default ReadList;
--- src/ReadBook/ReadBook.jsx
+++ src/ReadBook/ReadBook.jsx
@@ -1,28 +1,39 @@
 import React from "react";
 import { HiMiniUserGroup } from "react-icons/hi2";
 const ReadBook = ({ book }) => {
-  const { image, bookName, author, bookId, tags, publisher, category, rating } =
-    book;
+  const { image, bookName, author, tags, publisher, category, rating } = book;
   return (
-    <div className="border-2 border-gray-300 mb-5 p-4 rounded-lg shadow-md flex gap-3">
-      <div className="bg-gray-300 p-6 rounded-lg">
-        <img className="w-[150px]" src={image} alt="" />
+    <div className="border-1 border-gray-300 mb-5 p-4 rounded-lg shadow-md flex lg:flex-row md:flex-row flex-col gap-3 lg:w-[60%] mx-auto">
+      <div className="lg:bg-gray-300 md:bg-gray-300 lg:p-6 p-3 md:text-left mx-auto rounded-lg bg-gray-300">
+        <img className="w-[100px]" src={image} alt="" />
       </div>
 
       <div className="work">
-        <h1 className="text-[24px]">{bookName}</h1>
+        <h1 className="lg:text-[24px] md:text-[24px] font-bold play">
+          {bookName}
+        </h1>
         <h1>By : {author}</h1>
-        <div className="flex justify-start items-center gap-3 mt-4">
-            <span className="font-bold">Tags : </span>
-            {
-           tags.map(tag => <button className="text-[#23BE0A] bg-[#daecd8] px-3  rounded-2xl py-1">{tag}</button>)
-        }
+        <div className="flex justify-start items-center gap-3 lg:mt-4 md:mt-4 mt-2">
+          <span className="font-bold">Tags : </span>
+          {tags.map((tag) => (
+            <button className="text-[#23BE0A] bg-[#daecd8] px-3  rounded-2xl py-1">
+              {tag}
+            </button>
+          ))}
         </div>
-        <p className="flex items-center gap-2 mt-3 "><HiMiniUserGroup size={20}/> Publisher :  {publisher}</p>
-        <div className="flex gap-5 mt-3">
-            <button className="bg-[#e0effe] text-[#328EFF] px-3 py-1 rounded-2xl">Category: <span className="">{category}</span></button>
-            <button className="bg-[#fff2e0] text-[#FFAC33] px-3 py-1 rounded-2xl">Rating : {rating}</button>
-            <butto className='bg-[#23BE0A] text-white px-3 py-1 rounded-2xl'>View Details</butto>
+        <p className="flex items-center gap-2 md:mt-3 lg:mt-3 mt-2">
+          <HiMiniUserGroup size={20} /> Publisher : {publisher}
+        </p>
+        <div className="flex gap-5 lg:mt-3 md:mt-3 mt-2 ">
+          <button className="bg-[#e0effe] text-[#328EFF] lg:px-3 lg:py-1 rounded-2xl lg:text-[16px] md:text-[14px] text-[10px] flex items-center gap-2">
+            <span className="">{category}</span>
+          </button>
+          <button className="bg-[#fff2e0] text-[#FFAC33] px-3 py-1 rounded-2xl">
+            Rating : {rating}
+          </button>
+          <butto className="bg-[#23BE0A] text-white px-3 py-1 rounded-2xl">
+            View Details
+          </butto>
         </div>
       </div>
     </div>
